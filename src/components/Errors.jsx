681a92c414ff5c271b7a3b582ecd26499c12d589
John Garzon <johnharold.garzon795@comunidadunir.net>
2023-12-19 17:20:10 -0500

    Se crea la valdación del formulario y se crea el com ponente errores!

---
+++ src/components/Errors.jsx
@@ -0,0 +1,10 @@
+
+const Errors = ({children}) => {
+  return (
+      <div className="bg-red-600 text-white p-3 font-extralight rounded-md text-center my-5">
+      {children}
+    </div>
+  )
+}
+
+export default Errors
--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,16 +1,25 @@
-import { Form, useNavigate } from 'react-router-dom'
+import { Form, useNavigate, useActionData } from 'react-router-dom'
 import Formulario from '../components/Formulario';
+import Errors from '../components/Errors';
 
 export async function action({request}) { 
-  const formData = await request.formData(request.body);
-  console.log(formData);
-  return formData;
+  const formData = await request.formData();
+  const datos = Object.fromEntries(formData);
+
+  //Validando el formulario
+  const errors = [];
+  if (Object.values(datos).includes('')) { 
+    errors.push({message: 'All fields are required'});
+  }
+
+  return errors.length > 0 ? errors : null;
 }
 
 const NuevoCliente = () => {
+  const errors = useActionData();
   const navigate = useNavigate();
 
-
+  console.log(errors);
   return (
     <>
       <h1 className='text-4xl font-semibold mb-4 text-blue-900'>New Client</h1>
@@ -20,6 +29,7 @@ const NuevoCliente = () => {
         <button className='bg-blue-800 text-white px3 py-1 font-bold uppercase rounded p-3' onClick={() => navigate('/')}>Volver</button>
       </div>
       <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10'>
+        {errors?.length && errors.map((error, i) => (<Errors key={i}>{error.message}</Errors>))}
         <Form method='post'>
           <Formulario />
           <input type="submit" className='bg-blue-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-blue-900' value='Add Client' />
