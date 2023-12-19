32637b0694259116a8b0594d435c014337c9c3bf
John Garzon <johnharold.garzon795@comunidadunir.net>
2023-12-19 12:38:52 -0500

    Se crea el componente de Formulario y se crea el formulario.

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom'
+import Formulario from '../components/Formulario';
 
 
 const NuevoCliente = () => {
@@ -13,78 +14,8 @@ const NuevoCliente = () => {
       <div className='flex justify-end mt-10'>
         <button className='bg-blue-800 text-white px3 py-1 font-bold uppercase rounded p-3' onClick={() => navigate('/')}>Volver</button>
       </div>
-      <div className='flex justify-center mt-10'>
-        <div className='w-full max-w-3xl'>
-          <form
-            className='bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4'
-            action=''
-          >
-            <div className='mb-4'>
-              <label
-                className='block text-gray-700 text-sm font-bold mb-2'
-                htmlFor='nombre'
-              >
-                Name
-              </label>
-              <input
-                className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                type='text'
-                placeholder='Name'
-                id='nombre'
-              />
-            </div>
-            <div className='mb-4'>
-              <label
-                className='block text-gray-700 text-sm font-bold mb-2'
-                htmlFor='apellido'
-              >
-                Last Name
-              </label>
-              <input
-                className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                type='text'
-                placeholder='Last Name'
-                id='apellido'
-              />
-            </div>
-            <div className='mb-4'>
-              <label
-                className='block text-gray-700 text-sm font-bold mb-2'
-                htmlFor='empresa'
-              >
-                Company
-              </label>
-              <input
-                className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                type='text'
-                placeholder='Company'
-                id='empresa'
-              />
-            </div>
-            <div className='mb-4'>
-              <label
-                className='block text-gray-700 text-sm font-bold mb-2'
-                htmlFor='email'
-              >
-                Email
-              </label>
-              <input
-                className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                type='email'
-                placeholder='Email'
-                id='email'
-              />
-            </div>
-            <div className='mb-4'>
-              <label
-                className='block text-gray-700 text-sm font-bold mb-2'
-                htmlFor='telefono'
-              >
-                Phone
-              </label>
-            </div>
-          </form>
-        </div>
+      <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10'>
+        <Formulario />
       </div>
     </>
   )
